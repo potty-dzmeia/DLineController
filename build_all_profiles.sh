4ed6b9b32014e05a4daf113fb1c86c4ac3a96b7a
empotty-dzmeia/em <emch.levkov@gmail.com/em>
2014-11-25 14:27:14 +0100

    build_all_profiles.sh was corrected

--- build_all_profiles.sh
+++ build_all_profiles.sh
@@ -1,2 +1,29 @@
-#!/bin/bash          
-mvn -P win32 clean install
+#!/bin/bash    
+
+#Available build profiles:
+# win32
+# win64
+# linux_x86
+# linux_x86_64
+# linux_ia64
+# mac_os_x
+
+declare -a profiles=("win32" "win64" "linux_x86" "linux_x86_64" "linux_ia64" "mac_os_x")
+
+# make the dir where the jar files will be copied
+mkdir distribution
+
+for i in "${profiles[@]}"
+do
+   # 
+   echo "mvn -P "$i" clean install"
+   echo "-------------------------------------------------"
+   # build each of the profiles
+   mvn -P "$i" clean install
+   # copy the jar file to /distribution 
+   cp ./target/dlinecontroller-"$i".jar ./distribution/
+   
+done
+
+
+
